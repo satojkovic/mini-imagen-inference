334cef8e22ab8f1a431daa42f99668a33e4d5c84
satojkovic <satojkovic@gmail.com>
2024-01-23 23:25:24 +0900

    Add register_buffer

--- minimagen/diffusion_model.py
+++ minimagen/diffusion_model.py
@@ -17,3 +17,13 @@ class GaussianDiffusion(nn.Module):
         alphas = 1.0 - betas
         alphas_cumprod = torch.cumprod(alphas, axis=0)
         alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+
+        # register_buffer make sure that parameters don't get updated during backprop.
+        register_buffer = lambda name, val: self.register_buffer(
+            name, val.to(torch.float32), persistent=False
+        )
+
+        # Register variance schedule
+        register_buffer("betas", betas)
+        register_buffer("alphas_cumprod", alphas_cumprod)
+        register_buffer("alphas_cumprod_prev", alphas_cumprod_prev)
